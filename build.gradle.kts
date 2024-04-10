cc451264407511c2e40010854df66da3f38ffb57
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2024-04-10 21:18:30 +0300

    VER0.7: FIGURED IT OUT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import sun.tools.jar.resources.jar
-
 plugins {
     id("java")
     id("java-library")
@@ -15,34 +13,41 @@ subprojects {
         mavenCentral()
     }
 }
+
 project(":server") {
     apply(plugin = "application")
-    mainClass.set("server")
+    configure<JavaApplication> {
+        mainClass = "server"
+    }
     dependencies {
         implementation(project(":common"))
         implementation("com.google.code.gson:gson:2.10.1")
     }
-    jar {
+    tasks.withType<Jar>{
         manifest {
-            attributes 'Main-Class' : 'server.App'
+            attributes ["Main-Class"] = "server.App"
         }
     }
 }
+
 project(":client") {
     apply(plugin = "application")
-    mainClass.set("client.App")
+    configure<JavaApplication> {
+        mainClass = "client"
+    }
     dependencies {
         implementation(project(":common"))
     }
-    run {
-        standardInput = System.in
+    tasks.withType<JavaExec>{
+        standardInput = System.`in`
     }
-    jar {
+    tasks.withType<Jar> {
         manifest {
-            attributes 'Main-Class': 'client.App'
+            attributes["Main-Class"] = "client.App"
         }
     }
 }
+
 group = "ru.itmo.se"
 version = "2.0"
 description = "Lab6"
@@ -68,14 +73,12 @@ publishing {
         }
     }
 }
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 tasks.withType<Javadoc>{
     options.encoding = "UTF-8"
 }
-
 tasks.test {
     useJUnitPlatform()
 }
